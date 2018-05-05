6c70f07b7292278c5e2936caa8bae0295419fa95
Nobot_ <Gustav.bohlmark@gmail.com>
2018-05-05 12:35:26 +0200

    New coded scenario
    Added scenario to view and printer printout

--- src/Integration/Printer.java
+++ src/Integration/Printer.java
@@ -9,8 +9,10 @@ public class Printer {
     public Printer(){}
     public void printReceipt(Receipt receipt){
         String[] receiptPrint = receipt.getReceipt();
+        System.out.println("\n*Printing receipt*");
+        System.out.println("--Receipt--");
         for(int i= 0; i < receiptPrint.length; i++)
         System.out.println(receiptPrint[i]);
-
+        System.out.println("--Receipt--\n");
     }
 }
--- src/View/View.java
+++ src/View/View.java
@@ -15,8 +15,11 @@ public class View {
 
 
         controller.startSale();
+        System.out.println("Enter choklad id");
         saleDTO = controller.enterItem(3);
+        System.out.println("Enter bacon id");
         saleDTO = controller.enterItem(2);
+        System.out.println("Total cost is: "+saleDTO.getTotalCost());
         System.out.println("Payment: 53");
 
         change = controller.finishSale(53);
